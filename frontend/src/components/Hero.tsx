689c22efa80589685815590536b826e39d2a36f8
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-27 21:07:58 +0530

    home ui update

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -7,10 +7,10 @@ import { BackgroundLines } from "@/components/ui/background-lines";
 function Hero() {
   return (
     <BackgroundBeamsWithCollision
-      className="h-[90vh] md:h-screen relative w-full overflow-hidden bg-neutral-950 gap-7 flex flex-col items-center justify-center p-10
+      className="h-[90vh] md:h-screen relative w-full overflow-hidden bg-neutral-950 gap-7 flex flex-col items-center justify-center p-5
         "
     >
-      <BackgroundLines className="h-[90vh] md:h-screen relative w-full overflow-hidden bg-neutral-950 flex gap-12 flex-col items-center justify-center">
+      <BackgroundLines className="h-[90vh] md:h-screen relative w-full overflow-hidden bg-neutral-950 flex gap-10 md:gap-12 flex-col md:items-center md:justify-center items-center justify-center">
         <div className="absolute inset-0 w-full h-full bg-neutral-950 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none " />
 
         <TextAnimate
@@ -19,7 +19,7 @@ function Hero() {
           duration={0.3}
           delay={0.5}
           once={false}
-          className="text-3xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans mt-20 "
+          className="text-5xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans mt-20 "
         >
           Let's Code ,
         </TextAnimate>
@@ -30,11 +30,11 @@ function Hero() {
           duration={0.3}
           delay={0.5}
           once={false}
-          className="text-3xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans  "
+          className="text-4xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans  "
         >
           Create & Celebrate Together
         </TextAnimate>
-        <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans">
+        <h1 className="text-5xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans">
           <ColourfulText text="NEXGEN" />
         </h1>
       </BackgroundLines>
