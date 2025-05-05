6abb8d35df881182f6008add68baafb190292426
kishanghosh090 <kishanghosh090@gmail.com>
2025-05-06 00:33:09 +0530

    added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,12 +7,12 @@ import { useLocation } from "react-router-dom";
 
 export default function Home() {
   const { pathname } = useLocation();
-        useEffect(() => {
-          window.scrollTo({
-            top: 0,
-            behavior: "smooth", // or "auto"
-          });
-        }, [pathname]);
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // or "auto"
+    });
+  }, [pathname]);
   return (
     <>
       <div className="bg-neutral-950 ">
